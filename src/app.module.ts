c60e8f005b24010883a9174c58d0a1e97d0a68fe
Gabriel <hernandezg.alvarez@gmail.com>
2025-06-05 17:25:10 -0300

    change password by env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { StkItemModule } from './stk-item/stk-item.module';
 import { StkExistenciaModule } from './stk-existencia/stk-existencia.module';
@@ -7,18 +8,29 @@ import { StkPrecioModule } from './stk-precio/stk-precio.module';
 import { BasMonedaModule } from './bas-moneda/bas-moneda.module';
 import { SysImageModule } from './sys_image/sys_image.module';
 import { StkFamiliaModule } from './stk_familia/stk_familia.module';
+import { MapsModule } from './maps/maps.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: 'mysql',
-    database: 'wetech',
-    autoLoadEntities: true,
-    synchronize: true,
-  }),StkItemModule, StkExistenciaModule, StkDepositoModule, StkPrecioModule, BasMonedaModule, SysImageModule, StkFamiliaModule],
-
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: process.env.ROOT_PASSWORD,
+      database: 'wetech',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    StkItemModule,
+    StkExistenciaModule,
+    StkDepositoModule,
+    StkPrecioModule,
+    BasMonedaModule,
+    SysImageModule,
+    StkFamiliaModule,
+    MapsModule,
+  ],
 })
 export class AppModule {}
