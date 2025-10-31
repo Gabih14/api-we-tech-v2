d8c6c8a9963fefa02a456baccf9ebe4cc5d83995
Gabriel <hernandezg.alvarez@gmail.com>
2025-10-31 18:57:20 -0300

    fix exports in vta comp item module

--- src/vta-comprobante/vta-comprobante.module.ts
+++ src/vta-comprobante/vta-comprobante.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { VtaComprobanteService } from './vta-comprobante.service';
 import { VtaComprobanteController } from './vta-comprobante.controller';
 import { VtaComprobante } from './entities/vta-comprobante.entity';
+import { VtaComprobanteItemModule } from 'src/vta-comprobante-item/vta-comprobante-item.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([VtaComprobante])],
+  imports: [TypeOrmModule.forFeature([VtaComprobante]), VtaComprobanteItemModule],
   controllers: [VtaComprobanteController],
   providers: [VtaComprobanteService],
   exports: [VtaComprobanteService],
