d87f2afa2a7631cadc8b076435e949a17bdd9f35
Gabriel <hernandezg.alvarez@gmail.com>
2026-02-20 15:43:16 -0300

    add observaciones

--- src/stk-item/entities/stk-item.entity.ts
+++ src/stk-item/entities/stk-item.entity.ts
@@ -5,47 +5,50 @@ import {
   OneToMany,
   ManyToOne,
   JoinColumn,
-} from "typeorm";
+} from 'typeorm';
 
-import { StkExistencia } from "../../stk-existencia/entities/stk-existencia.entity";
-import { StkPrecio } from "../../stk-precio/entities/stk-precio.entity";
-import { StkFamilia } from "../../stk_familia/entities/stk_familia.entity";
+import { StkExistencia } from '../../stk-existencia/entities/stk-existencia.entity';
+import { StkPrecio } from '../../stk-precio/entities/stk-precio.entity';
+import { StkFamilia } from '../../stk_familia/entities/stk_familia.entity';
 
-@Index("grupo", ["grupo"], {})
-@Index("subgrupo", ["subgrupo"], {})
-@Entity("stk_item")
+@Index('grupo', ['grupo'], {})
+@Index('subgrupo', ['subgrupo'], {})
+@Entity('stk_item')
 export class StkItem {
-  @Column("varchar", { primary: true, name: "id", length: 20 })
+  @Column('varchar', { primary: true, name: 'id', length: 20 })
   id: string;
 
-  @Column("varchar", { name: "descripcion", nullable: true, length: 2048 })
+  @Column('varchar', { name: 'descripcion', nullable: true, length: 2048 })
   descripcion: string | null;
 
-  @Column("varchar", { name: "presentacion", nullable: true, length: 100 })
+  @Column('varchar', { name: 'presentacion', nullable: true, length: 100 })
   presentacion: string | null;
 
-  @Column("enum", {
-    name: "tipo",
-    nullable: true,
-    enum: ["PT", "SE", "MP", "CP", "BU", "S", "C"],
-  })
-  tipo: "PT" | "SE" | "MP" | "CP" | "BU" | "S" | "C" | null;
+  @Column('text', { name: 'observaciones', nullable: true })
+  observaciones: string | null;
 
-  @Column("varchar", { name: "grupo", nullable: true, length: 50 })
+  @Column('enum', {
+    name: 'tipo',
+    nullable: true,
+    enum: ['PT', 'SE', 'MP', 'CP', 'BU', 'S', 'C'],
+  })
+  tipo: 'PT' | 'SE' | 'MP' | 'CP' | 'BU' | 'S' | 'C' | null;
+
+  @Column('varchar', { name: 'grupo', nullable: true, length: 50 })
   grupo: string | null;
 
-  @Column("varchar", { name: "subgrupo", nullable: true, length: 50 })
+  @Column('varchar', { name: 'subgrupo', nullable: true, length: 50 })
   subgrupo: string | null;
 
   /** 🔥 Clave foránea real en la DB: "familia" (no "familia_id") */
-  @Column("varchar", { name: "familia", nullable: true, length: 20 })
+  @Column('varchar', { name: 'familia', nullable: true, length: 20 })
   familia: string | null;
 
   @ManyToOne(() => StkFamilia, (stkFamilia) => stkFamilia.stkItems, {
-    onDelete: "SET NULL",
-    onUpdate: "CASCADE",
+    onDelete: 'SET NULL',
+    onUpdate: 'CASCADE',
   })
-  @JoinColumn([{ name: "familia", referencedColumnName: "id" }])
+  @JoinColumn([{ name: 'familia', referencedColumnName: 'id' }])
   familia2: StkFamilia;
 
   @OneToMany(() => StkExistencia, (stkExistencia) => stkExistencia.item2)
@@ -54,19 +57,17 @@ export class StkItem {
   @OneToMany(() => StkPrecio, (stkPrecio) => stkPrecio.item2)
   stkPrecios: StkPrecio[];
 
-@Column({
-  type: 'bit',
-  width: 1,
-  nullable: true,
-  transformer: {
-    to: (value: boolean) => (value ? 1 : 0),
-    from: (value: Buffer) => value?.[0] === 1,
-  },
-})
-visible: boolean;
+  @Column({
+    type: 'bit',
+    width: 1,
+    nullable: true,
+    transformer: {
+      to: (value: boolean) => (value ? 1 : 0),
+      from: (value: Buffer) => value?.[0] === 1,
+    },
+  })
+  visible: boolean;
 
-
-  @Column("blob", { name: "foto", nullable: true })
+  @Column('blob', { name: 'foto', nullable: true })
   foto: Buffer | null;
-
 }
